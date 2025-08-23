0e8f54a80878beb185d1307ae2a59cdebd580857
Trogris <cwa.andrade@gmail.com>
2025-08-23 18:16:06 -0300

    Update loaders.py

--- loaders.py
+++ loaders.py
@@ -1,50 +1,63 @@
 import os
 from time import sleep
 import streamlit as st
-from langchain_community.document_loaders import (WebBaseLoader,
-                                                  YoutubeLoader, 
-                                                  CSVLoader, 
-                                                  PyPDFLoader, 
-                                                  TextLoader)
+from langchain_community.document_loaders import (
+    WebBaseLoader,
+    YoutubeLoader,
+    CSVLoader,
+    PyPDFLoader,
+    TextLoader
+)
 from fake_useragent import UserAgent
 
-def carrega_site(url):
+
+def carrega_site(url: str) -> str:
+    """
+    Tenta carregar o conteúdo de uma página com até 5 tentativas,
+    alternando o USER_AGENT para reduzir bloqueios simples.
+    """
     documento = ''
     for i in range(5):
         try:
             os.environ['USER_AGENT'] = UserAgent().random
             loader = WebBaseLoader(url, raise_for_status=True)
-            lista_documentos = loader.load()
-            documento = '\n\n'.join([doc.page_content for doc in lista_documentos])
+            docs = loader.load()
+            documento = '\n\n'.join([doc.page_content for doc in docs])
             break
-        except:
-            print(f'Erro ao carregar o site {i+1}')
+        except Exception:
+            print(f'Erro ao carregar o site (tentativa {i+1})')
             sleep(3)
+
     if documento == '':
-        st.error('Não foi possível carregar o site')
+        st.error('Não foi possível carregar o site. Verifique a URL ou tente novamente.')
         st.stop()
     return documento
 
-def carrega_youtube(video_id):
-    loader = YoutubeLoader(video_id, add_video_info=False, language=['pt'])
-    lista_documentos = loader.load()
-    documento = '\n\n'.join([doc.page_content for doc in lista_documentos])
-    return documento
 
-def carrega_csv(caminho):
+def carrega_youtube(video_url_or_id: str) -> str:
+    """
+    Aceita URL completa do YouTube ou ID do vídeo.
+    """
+    loader = YoutubeLoader(video_url_or_id, add_video_info=False, language=['pt'])
+    docs = loader.load()
+    return '\n\n'.join([doc.page_content for doc in docs])
+
+
+def carrega_csv(caminho: str) -> str:
     loader = CSVLoader(caminho)
-    lista_documentos = loader.load()
-    documento = '\n\n'.join([doc.page_content for doc in lista_documentos])
-    return documento
+    docs = loader.load()
+    return '\n\n'.join([doc.page_content for doc in docs])
 
-def carrega_pdf(caminho):
+
+def carrega_pdf(caminho: str) -> str:
     loader = PyPDFLoader(caminho)
-    lista_documentos = loader.load()
-    documento = '\n\n'.join([doc.page_content for doc in lista_documentos])
-    return documento
+    docs = loader.load()
+    return '\n\n'.join([doc.page_content for doc in docs])
 
-def carrega_txt(caminho):
+
+def carrega_txt(caminho: str) -> str:
     loader = TextLoader(caminho)
-    lista_documentos = loader.load()
-    documento = '\n\n'.join([doc.page_content for doc in lista_documentos])
-    return documento
+    docs = loader.load()
+    return '\n\n'.join([doc.page_content for doc in docs])
+
+
